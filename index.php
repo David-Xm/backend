0a77387eba457d45167ed33b46dcd1becd32ea98
davidxm <anthonydavid6508@gmail.com>
2025-12-04 13:09:02 +0100

    php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+echo $_POST['username'];
+echo $_POST['password'];
+
+
+put://input
+?>; 
+
+
+
+<html>
+    <head>
+        <title>
+            Backend Class
+        </title>
+    </head>
+    <br><br><br>
+        My name is Davidxm, i am learning Backend Development.
+        <br><br>
+        <form action="davidxm.php" method="post">
+            <input type="text" name="username" placeholder="Enter Username">
+            <input type="password" name="password" placeholder="Enter Password">
+            <input type="submit" value="Login">
+        </form>
+</html>
